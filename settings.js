0ecb2aa580a32576cdd3422120afee45dde914d1
nuwanmusicbot <110467597+nuwanmusicbot@users.noreply.github.com>
2023-08-10 00:49:15 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -27,7 +27,7 @@ global.CAPTION = '*© ɢᴇɴᴀʀᴀᴛᴇᴅ ʙʏ @📍 M ɾ.  𝗗 𝗔 𝗥 
 📌⭕ 𝗬𝗢𝗨 𝗠𝗨𝗦𝗧 𝗣𝗥𝗢𝗩𝗜𝗗𝗘 𝗕𝗘𝗙𝗢𝗥𝗘 𝗗𝗘𝗣𝗟𝗢𝗬 𝗕𝗢𝗧 📌⭕
 > Ai Bot Password Get From Watching This Video 
 */
-global.AI_PASSWORD = ''
+global.AI_PASSWORD = 'king-nima-gpt-bot'
 global.SESSION_ID = ''
 
 /*
